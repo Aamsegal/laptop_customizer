505de3125e06591762053d3125ba2a6c4c7a48fa
Amsegal93 <Amsegal93@gmail.com>
2019-07-21 17:29:55 -0400

    Refactored code to be more react friendly. Not all of the calculations and rendering happen in one file. Its spread out in to smaller modular files with single purposes.

---
+++ src/Summary/Summary.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Summary extends Component {
+    render() {
+        const selected = this.props.selected
+        return (
+            Object.keys(selected)
+            .map(key => <div className="summary__option" key={key}>
+            <div className="summary__option__label">{key}  </div>
+            <div className="summary__option__value">{this.props.selected[key].name}</div>
+            <div className="summary__option__cost">
+              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                  .format(this.props.selected[key].cost) }
+            </div>
+        </div>)
+        )
+    }
+}
+
+export default Summary;
---
+++ src/Total/Total.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+
+/*const total = Object.keys(this.state.selected)
+          .reduce((acc, curr) => acc + this.state.selected[curr].cost, 0);
+
+export default Total;*/
